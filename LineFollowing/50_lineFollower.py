711526e1ebb933c16f33eb67fdfcad3c0a4b390d
Spencer Fishman <spencer@hurricanedevelopment.com>
2016-07-02 22:42:55 +0000

    Working line follower

--- LineFollowing/50_lineFollower.py
+++ LineFollowing/50_lineFollower.py
@@ -11,16 +11,26 @@ rpi.init()
 psm = rpi.psm
 
 prevLeftLight = True
-prevRightLight = False
+#prevRightLight = False
 
+pGain = 0.43 # How intense the turning will be
+basePower = 35 # Base speed regardless of turning
+offset = 500 # offset for the middle of light sensor data
 
 # Rudimentary line follower
 while (not psm.isKeyPressed()):
 	leftSens = psm.BBS1.lightSensorNXT()
-	rightSens = psm.BBS2.reflectedLightSensorEV3()
-	psm.screen.termPrintln(str(leftSens )+ "   :::   " + str(rightSens))
+#	rightSens = psm.BBS2.reflectedLightSensorEV3()
+	#psm.screen.termPrintln(str(leftSens )+ "   :::   " + str(rightSens))
+#	psm.screen.termPrint(str(leftSens))
+#	psm.screen.termPrint(" ::: ")
 
-	if (not debugOnly)
+	error = leftSens - offset
+#	psm.screen.termPrintln(str(error))
+	psm.BBM2.setSpeed(-1 * (-1 * error * pGain + basePower))
+	psm.BBM1.setSpeed(-1 * (error * pGain + basePower))
+'''
+	if (not debugOnly):
 		if prevLeftLight == isLight( leftSens ) and isLight( leftSens ) and prevRightLight == isLight( rightSens ) and isDark( rightSens ):
 			#Keep going straight, maybe increase speed
 			psm.BBM1.setSpeedSync(50)
@@ -32,7 +42,7 @@ while (not psm.isKeyPressed()):
 			#Was straight, now is right of the path
 			psm.BBM1.setSpeed(25)
 			psm.BBM1.setSpeed(50)
-
-		prevLeftLight = isLight( leftSens )
-		prevRightLight = isLight( rightSens )
+'''
+		#prevLeftLight = isLight( leftSens )
+		#prevRightLight = isLight( rightSens )
 rpi.dinit()
