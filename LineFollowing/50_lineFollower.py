42b5596b1c579362a9202df4ac29c92a4e06cdb0
superfunnytogo <superfunnytogo@gmail.com>
2016-07-02 09:50:44 -0400

    Working version of line follower

--- LineFollowing/50_lineFollower.py
+++ LineFollowing/50_lineFollower.py
@@ -1,8 +1,18 @@
 import Rpi as rpi
 
+def isDark( val ):
+	return val >= 50 and val <= 70
+def isLight( val ):
+	return val >= 35 and val <= 50
+
 debugOnly = True
 
 rpi.init()
+psm = rpi.psm
+
+prevLeftLight = True
+prevRightLight = False
+
 
 # Rudimentary line follower
 while (not psm.isKeyPressed()):
@@ -11,7 +21,18 @@ while (not psm.isKeyPressed()):
 	psm.screen.termPrintln(str(leftSens )+ "   :::   " + str(rightSens))
 
 	if (not debugOnly)
-		psm.BBM1.setSpeed(50)
-		psm.BBM1.setSpeed
+		if prevLeftLight == isLight( leftSens ) and isLight( leftSens ) and prevRightLight == isLight( rightSens ) and isDark( rightSens ):
+			#Keep going straight, maybe increase speed
+			psm.BBM1.setSpeedSync(50)
+		elif  prevLeftLight == isLight( leftSens ) and isLight( leftSens ) and  not prevRightLight and isLight( rightSens ):
+			#Was straight, now is left of the path
+			psm.BBM1.setSpeed(50)
+			psm.BBM2.setSpeed(25)
+		elif  prevLeftLight  and isDark( leftSens ):
+			#Was straight, now is right of the path
+			psm.BBM1.setSpeed(25)
+			psm.BBM1.setSpeed(50)
 
+		prevLeftLight = isLight( leftSens )
+		prevRightLight = isLight( rightSens )
 rpi.dinit()
