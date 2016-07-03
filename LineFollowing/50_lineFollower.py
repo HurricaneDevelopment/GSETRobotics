816181c45063304910767f362c1e1a9ad53b8b74
superfunnytogo <superfunnytogo@gmail.com>
2016-07-02 23:04:53 -0400

    Added Integral

--- LineFollowing/50_lineFollower.py
+++ LineFollowing/50_lineFollower.py
@@ -1,48 +1,28 @@
 import Rpi as rpi
 
-def isDark( val ):
-	return val >= 50 and val <= 70
-def isLight( val ):
-	return val >= 35 and val <= 50
-
-debugOnly = True
-
 rpi.init()
 psm = rpi.psm
 
-prevLeftLight = True
-#prevRightLight = False
-
-pGain = 0.43 # How intense the turning will be
 basePower = 35 # Base speed regardless of turning
-offset = 500 # offset for the middle of light sensor data
+offset = 500 # Offset for the middle of light sensor data
+pGain = 0.43 # Proportional intensity
+iGain = 0.2
+iMax = 0;
+iMin = 0;
+
+totalError = 0
 
 # Rudimentary line follower
 while (not psm.isKeyPressed()):
-	leftSens = psm.BBS1.lightSensorNXT()
-#	rightSens = psm.BBS2.reflectedLightSensorEV3()
-	#psm.screen.termPrintln(str(leftSens )+ "   :::   " + str(rightSens))
-#	psm.screen.termPrint(str(leftSens))
-#	psm.screen.termPrint(" ::: ")
+	lightSens = psm.BBS1.lightSensorNXT()
 
-	error = leftSens - offset
-#	psm.screen.termPrintln(str(error))
-	psm.BBM2.setSpeed(-1 * (-1 * error * pGain + basePower))
-	psm.BBM1.setSpeed(-1 * (error * pGain + basePower))
-'''
-	if (not debugOnly):
-		if prevLeftLight == isLight( leftSens ) and isLight( leftSens ) and prevRightLight == isLight( rightSens ) and isDark( rightSens ):
-			#Keep going straight, maybe increase speed
-			psm.BBM1.setSpeedSync(50)
-		elif  prevLeftLight == isLight( leftSens ) and isLight( leftSens ) and  not prevRightLight and isLight( rightSens ):
-			#Was straight, now is left of the path
-			psm.BBM1.setSpeed(50)
-			psm.BBM2.setSpeed(25)
-		elif  prevLeftLight  and isDark( leftSens ):
-			#Was straight, now is right of the path
-			psm.BBM1.setSpeed(25)
-			psm.BBM1.setSpeed(50)
-'''
-		#prevLeftLight = isLight( leftSens )
-		#prevRightLight = isLight( rightSens )
+	#PI Controller
+	error = lightSens - offset
+	totalError += error
+	totalError = (totalError > iMax) ? iMax : (totalError < iMin) ? iMin : totalError
+	output = pGain * error + iGain * totalError
+
+	psm.BBM2.setSpeed(-1 * (-1 * output+ basePower))
+	psm.BBM1.setSpeed(-1 * (output + basePower))
+
 rpi.dinit()
